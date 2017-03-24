624bf1e78d232a3431f9935d01c75d57887ab8db
yari <yarlaz@gmail.com>
2017-03-24 16:21:36 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageListComponent,
+    MessageSectionComponent,
+    ThreadListComponent,
+    ThreadSectionComponent,
+    UserSelectionComponent
   ],
   imports: [
     BrowserModule,
